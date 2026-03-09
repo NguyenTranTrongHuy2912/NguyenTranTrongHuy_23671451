857d9b1aa5bb44af807b25c09ef81ec8d6a406a4
H82M19\Student <Student>
2026-03-09 07:54:07 +0700

    feat: implement data fetching from API and display user information

--- Tuan04/baitap01/src/App.jsx
+++ Tuan04/baitap01/src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  var url = "https://jsonplaceholder.typicode.com/users"
+
+  // useEffect(() => {
+  //   var res = fetch(url);
+
+  //   res.then((response) => {
+  //     return response.json();
+  //   }).then((datafetch) => {
+  //     setData(datafetch);
+  //     return datafetch;
+  //   })
+  // }, [])
+
+  useEffect(() => {
+    async function fetchData(params) {
+      var res = await fetch(url);
+      var datafetch = await res.json();
+      setData(datafetch);
+    }
+    fetchData();
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        data.map((item) => {
+          return <div key={item.id}>
+            <h2>{item.name}</h2>
+            <h2>{item.email}</h2>
+          </div>
+        })
+      }
+
     </>
   )
 }
