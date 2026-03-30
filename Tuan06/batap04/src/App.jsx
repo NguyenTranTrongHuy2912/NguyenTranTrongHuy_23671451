670953a90f499346b746be8bbb6b474a98b7bf4b
H82M19\Student <Student>
2026-03-30 08:34:54 +0700

    done bt04

--- Tuan06/batap04/src/App.jsx
+++ Tuan06/batap04/src/App.jsx
@@ -3,14 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Orders from './components/Orders'
+import Profile from './components/Profile'
+import Settings from './components/Settings'
+import NotFound from './components/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <nav>
+        <Link to='/'>Home</Link> |
+        <Link to='/dashboard'>Dashboard</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/dashboard' element={<Dashboard />}> 
+          <Route path='orders' element={<Orders/>}></Route>
+          <Route path='profile' element={<Profile/>}></Route>
+          <Route path='settings' element={<Settings/>}></Route>
+        </Route>
+        <Route path='*' element={<NotFound/>}></Route>
+
+      </Routes>
     </>
+
   )
 }
 
--- Tuan06/batap04/src/components/Dashboard.jsx
+++ Tuan06/batap04/src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 function Dashboard() {
   return (
     <div>
         Dashboard
+        <Outlet />
     </div>
+    
   )
 }
 
