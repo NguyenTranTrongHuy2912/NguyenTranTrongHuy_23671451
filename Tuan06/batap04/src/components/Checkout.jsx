e1e6fd5d3eeee4128e4f700a7026a06e6629206e
H82M19\Student <Student>
2026-03-30 08:51:59 +0700

    bt05 on progress...

---
+++ Tuan06/batap04/src/components/Checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div>Thanh toan thanh cong!</div>
+  )
+}
+
+export default Checkout
--- Tuan06/batap04/src/components/Dashboard.jsx
+++ Tuan06/batap04/src/components/Dashboard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+const navigate = useNavigate()
+console.log(navigate)
+function handleBuy() {
+    navigate('/dashboard/checkout')
+}
 function Dashboard() {
-  return (
-    <div>
-        Dashboard
-        <Outlet />
-    </div>
-    
-  )
+    return (
+        <div>
+            Dashboard
+            <Outlet />
+            <br />
+            <button onClick={handleBuy}>Mua hang</button>
+        </div>
+
+    )
 }
 
 export default Dashboard
