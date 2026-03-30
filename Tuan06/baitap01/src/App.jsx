99e4d46cfe86142de2be65de521b569f69464372
H82M19\Student <Student>
2026-03-30 07:46:58 +0700

    done bt1

--- Tuan06/baitap01/src/App.jsx
+++ Tuan06/baitap01/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <nav>
+        <Link to='/'>Home</Link> |
+        <Link to='/contact'>Contact</Link> |
+        <Link to='/about'>About</Link>
+      </nav>
+
       <Routes>
-        
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
     </>
   )
