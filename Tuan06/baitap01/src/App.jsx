f578bca620f713cde1a3b87446e9f6414a67f55e
H82M19\Student <Student>
2026-03-30 07:56:16 +0700

    done bt2

--- Tuan06/baitap01/src/App.jsx
+++ Tuan06/baitap01/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
   )
