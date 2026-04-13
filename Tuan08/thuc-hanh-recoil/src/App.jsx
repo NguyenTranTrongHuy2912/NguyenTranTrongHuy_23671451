43522ede9b307042a3da89a41769ac51931f2338
H82M19\Student <Student>
2026-04-13 08:01:23 +0700

    done ex use recoil to calc Area

---
+++ Tuan08/thuc-hanh-recoil/src/App.jsx
@@ -0,0 +1,41 @@
+import { use, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { height, width, area, cart } from './recoil/Atoms'
+import { useRecoilState, useRecoilValue } from 'recoil'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  var [h, setH] = useRecoilState(height);
+  var [w, setW] = useRecoilState(width);
+  var a = useRecoilValue(area);
+  var [myCart, setMyCart] = useRecoilState(cart);
+
+  console.log(myCart)
+  function handleChangeHeight() {
+    setH(h + 1)
+  }
+
+  function handleChangeWidth(){
+    setW(w + 1)
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Height: {h}</h2>
+        <h2>Width: {w}</h2>
+        <h2>Area: {a}</h2>
+        <div>
+          <button onClick={handleChangeHeight}>Change height</button>
+          <button onClick={handleChangeWidth}>Change height</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
